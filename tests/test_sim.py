e2b715b7abc1ccd39bca72f4505177609c3158f1
ej550 <edward.oughton@gmail.com>
2021-01-03 18:19:07 +0000

    Add tests

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -1,179 +1,95 @@
 import pytest
-from globalsat.sim import (
-    system_capacity,
-    calc_geographic_metrics,
-    calc_free_space_path_loss,
-    generate_log_normal_dist_value,
-    calc_antenna_gain,
-    calc_eirp,
-    calc_received_power,
-    calc_noise,
-    calc_cnr,
-    calc_spectral_efficiency,
-    calc_capacity,
-)
+import random
+from osira.sim import simulation, allocate_probabilities
 
 
-def test_system_capacity(setup_params, setup_lut):
+def test_simulation():
     """
-    Integration test for main system capacity function.
+    Integration test for the main simulation generation function.
 
     """
-    number_of_satellites = 10
+    random.seed(42)
 
-    results = system_capacity('starlink', number_of_satellites, setup_params, setup_lut)[0]
+    data = {}
 
-    assert results['number_of_satellites'] == 10
-    assert round(results['distance']) == 10
-    assert results['satelite_coverage_area'] == 8
-    assert results['iteration'] == 0
-    assert round(results['path_loss']) == 136
-    assert round(results['antenna_gain']) == 38
-    assert round(results['eirp']) == 64
-    assert round(results['received_power']) == -72
-    assert round(results['noise']) == -90
-    assert round(results['cnr']) == 18
-    assert results['spectral_efficiency'] == 4.735354
-    assert round(results['capacity']) == 1184
-    assert round(results['capacity_kmsq']) == 148
+    for i in range(0, 100):
+        data[i] = {
+            'id': i,
+            'population': random.randint(100, 1000)
+            }
+
+    num_substations = [4]
+    iterations = 100
+    probabilities = [0.5, 0.1, 0.01]
+
+    all_results = simulation(data, num_substations, probabilities, iterations)
+
+    assert len(all_results) == 100
+
+    max_iters = max([i['iteration'] for i in all_results])
+
+    assert max_iters == (iterations - 1) #Remember: iterations run from 0 to 99.
+
+    max_nodes = max([len(i['substation_ids']) for i in all_results])
+
+    assert max_nodes == num_substations[0]
+
+    unique_nodes = list(set([i['nodes'] for i in all_results]))
+
+    assert unique_nodes == num_substations
 
 
-def test_calc_geographic_metrics():
+def test_allocate_probabilities():
     """
-    Unit test for calculating geographic metrics including:
-
-    - Distance between the transmitter and reciever
-    - Coverage area for each satellite.
-
-    """
-    number_of_satellites = 10
-
-    params = {
-        'total_area_earth_km_sq': 100,
-        'portion_of_earth_covered': 0.8,
-        'altitude_km': 10,
-    }
-
-    # area_of_earth_covered = total_area_earth_km_sq * portion_of_earth_covered
-    # network_density = number_of_satellites / area_of_earth_covered
-    # satelite_coverage_area = area_of_earth_covered / number_of_satellites
-    # mean_distance_between_assets = math.sqrt((1 / network_density)) / 2
-    # distance = math.sqrt((mean_distance_between_assets**2) + (altitude_km**2))
-
-    # 80 km^2 = 100 * 0.8
-    # 0.125 satellites km^2 = 10 satellites / 80 km^2
-    # 8 km^2 satellite coverage area = 80 km^2 / 10 satellites
-    # 1.41 km on average = math.sqrt((1 / 0.125 km^2 )) / (2)
-    # 10.1 km = math.sqrt((1.41**2) + (10**2))
-
-    distance, satelite_coverage_area = calc_geographic_metrics(number_of_satellites, params)
-
-    assert round(distance) == 10
-    assert satelite_coverage_area == 8
-
-
-def test_calc_free_space_path_loss():
-    """
-    Unit test for calculating the free space path loss.
+    Unit test for allocating cumulative probabilities.
 
     """
-    distance = 10
-    params = {'speed_of_light': 300000000, 'dl_frequency': 13500000000}
-    i = 0
-    random_variations = [1]
+    random.seed(42)
 
-    result = calc_free_space_path_loss(distance, params, i, random_variations)
+    data = {}
 
-    assert round(result) == 136
+    for i in range(0, 100):
+        data[i] = {
+            'id': i,
+            'population': random.randint(100, 1000)
+            }
 
+    num_substations = [4]
+    iterations = 100
+    probabilities = [0.01]
 
-def test_generate_log_normal_dist_value():
-    """
-    Unit test for generating random values using a lognormal distribution.
+    all_results = simulation(data, num_substations, probabilities, iterations)
 
-    """
-    assert round(generate_log_normal_dist_value(13.5, 2, 1, 1, 1)[0]) == 1
-    assert round(generate_log_normal_dist_value(13.5, 2, 1, None, 1)[0]) == 2
+    cp_scenarios = allocate_probabilities(all_results, num_substations, probabilities)
 
+    assert len(cp_scenarios) == 1
+    assert cp_scenarios[0]['nodes'] == 4
+    assert cp_scenarios[0]['iteration'] == 96
+    assert cp_scenarios[0]['population'] == 3477
+    assert cp_scenarios[0]['substation_ids'] == [33, 26, 85, 91]
+    assert cp_scenarios[0]['rank'] == 1
+    assert cp_scenarios[0]['cum_probability'] == 0.01
 
-def test_antenna_gain():
-    """
-    Unit test for calculating the antenna gain.
+    data = {}
 
-    """
-    c = 3e8 #speed of light m/s
-    d = 0.7 #Antenna Diameter in metres
-    f = 13500000000 #Frequency in Hertz
-    n = 0.6 #Antenna Efficiency
+    for i in range(0, 100):
+        data[i] = {
+            'id': i,
+            'population': random.randint(100, 1000)
+            }
 
-    assert round(calc_antenna_gain(c, d, f, n)) == 38
+    num_substations = [7]
+    iterations = 100
+    probabilities = [0.5]
 
+    all_results = simulation(data, num_substations, probabilities, iterations)
 
-def test_calc_eirp():
-    """
-    Unit test for calculating the Equivalent Isotropically Radiated Power.
+    cp_scenarios = allocate_probabilities(all_results, num_substations, probabilities)
 
-    """
-    power = 40 # watts
-    antenna_gain = 38 # dB
-    losses = 4 #dB
-
-    assert round(calc_eirp(power, antenna_gain, losses)) == 74
-
-
-def test_calc_received_power():
-    """
-    Unit test for calculating received power.
-
-    """
-    eirp = 74 # dBi
-    path_loss = 136 # dB
-
-    assert round(calc_received_power(eirp, path_loss)) == -62
-
-
-def test_calc_noise():
-    """
-    Unit test for calculating receiver noise.
-
-    """
-    assert round(calc_noise()) == -90
-
-
-def test_calc_cnr():
-    """
-    Unit test for calculating the Carrier-to-Noise Ratio (CNR).
-
-    """
-    received_power = -62 # dB
-    noise = -90 # dB
-
-    assert round(calc_cnr(received_power, noise)) == 28
-
-
-def test_calc_spectral_efficiency():
-    """
-    Unit test for finding the spectral efficnecy.
-
-    """
-    lut = [
-        (0, 1),
-        (10, 2),
-        (20, 4),
-    ]
-
-    assert calc_spectral_efficiency(-2, lut) == 1 # bits/Hz/s
-    assert calc_spectral_efficiency(2, lut) == 1 # bits/Hz/s
-    assert calc_spectral_efficiency(15, lut) == 2 # bits/Hz/s
-    assert calc_spectral_efficiency(28, lut) == 4 # bits/Hz/s
-
-
-def test_calc_capacity():
-    """
-    Unit test for finding the spectral efficnecy.
-
-    """
-    spectral_efficiency = 1 # bits per Hertz
-    dl_bandwidth = 1e6 # Hertz
-
-    assert calc_capacity(spectral_efficiency, dl_bandwidth) == 1 #mbps
+    assert len(cp_scenarios) == 1
+    assert cp_scenarios[0]['nodes'] == 7
+    assert cp_scenarios[0]['iteration'] == 92
+    assert cp_scenarios[0]['population'] == 3551
+    assert cp_scenarios[0]['substation_ids'] == [89, 49, 63, 51, 31, 18, 83]
+    assert cp_scenarios[0]['rank'] == 50
+    assert cp_scenarios[0]['cum_probability'] == 0.495
